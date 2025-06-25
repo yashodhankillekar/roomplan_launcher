a9053c6aac025f85ca2fbae0b4beb8c8815df650
Yashodhan Killekar <yashodhankillekar@gmail.com>
2025-06-25 19:16:26 +0530

    updated the code

--- ios/Classes/RoomCaptureViewController.swift
+++ ios/Classes/RoomCaptureViewController.swift
@@ -1,99 +1,133 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+The sample app's main view controller that manages the scanning process.
+*/
 
 import UIKit
 import RoomPlan
 
-@objc public class RoomCaptureViewController: UIViewController, RoomCaptureViewDelegate, RoomCaptureSessionDelegate {
-
-    private var isScanning = false
+class RoomCaptureViewController: UIViewController, RoomCaptureViewDelegate, RoomCaptureSessionDelegate {
+    
+    @IBOutlet var exportButton: UIButton?
+    
+    @IBOutlet var doneButton: UIBarButtonItem?
+    @IBOutlet var cancelButton: UIBarButtonItem?
+    @IBOutlet var activityIndicator: UIActivityIndicatorView?
+    
+    private var isScanning: Bool = false
+    
     private var roomCaptureView: RoomCaptureView!
-    private var roomCaptureSessionConfig = RoomCaptureSession.Configuration()
+    private var roomCaptureSessionConfig: RoomCaptureSession.Configuration = RoomCaptureSession.Configuration()
+    
     private var finalResults: CapturedRoom?
-
-    private let exportButton = UIButton(type: .system)
-    private let activityIndicator = UIActivityIndicatorView(style: .large)
-
-    public override func viewDidLoad() {
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
+        
+        // Set up after loading the view.
         setupRoomCaptureView()
-        activityIndicator.stopAnimating()
+        activityIndicator?.stopAnimating()
     }
-
-    private func setupUI() {
-        view.backgroundColor = .white
-
-        exportButton.setTitle("Export", for: .normal)
-        exportButton.isEnabled = false
-        exportButton.addTarget(self, action: #selector(exportResults), for: .touchUpInside)
-
-        activityIndicator.center = view.center
-        view.addSubview(activityIndicator)
-
-        exportButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(exportButton)
-        NSLayoutConstraint.activate([
-            exportButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            exportButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
-        ])
-    }
-
+    
     private func setupRoomCaptureView() {
         roomCaptureView = RoomCaptureView(frame: view.bounds)
         roomCaptureView.captureSession.delegate = self
         roomCaptureView.delegate = self
+        
         view.insertSubview(roomCaptureView, at: 0)
     }
-
-    public override func viewDidAppear(_ animated: Bool) {
+    
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         startSession()
     }
-
-    public override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+    
+    override func viewWillDisappear(_ flag: Bool) {
+        super.viewWillDisappear(flag)
         stopSession()
     }
-
+    
     private func startSession() {
         isScanning = true
-        roomCaptureView.captureSession.run(configuration: roomCaptureSessionConfig)
+        roomCaptureView?.captureSession.run(configuration: roomCaptureSessionConfig)
+        
+        setActiveNavBar()
     }
-
+    
     private func stopSession() {
         isScanning = false
-        roomCaptureView.captureSession.stop()
+        roomCaptureView?.captureSession.stop()
+        
+        setCompleteNavBar()
     }
-
-    public func captureView(shouldPresent roomDataForProcessing: CapturedRoomData, error: Error?) -> Bool {
+    
+    // Decide to post-process and show the final results.
+    func captureView(shouldPresent roomDataForProcessing: CapturedRoomData, error: Error?) -> Bool {
         return true
     }
-
-    public func captureView(didPresent processedResult: CapturedRoom, error: Error?) {
+    
+    // Access the final post-processed results.
+    func captureView(didPresent processedResult: CapturedRoom, error: Error?) {
         finalResults = processedResult
-        exportButton.isEnabled = true
-        activityIndicator.stopAnimating()
+        self.exportButton?.isEnabled = true
+        self.activityIndicator?.stopAnimating()
+    }
+    
+    @IBAction func doneScanning(_ sender: UIBarButtonItem) {
+        if isScanning { stopSession() } else { cancelScanning(sender) }
+        self.exportButton?.isEnabled = false
+        self.activityIndicator?.startAnimating()
     }
 
-    @objc private func exportResults() {
-        guard let finalResults = finalResults else { return }
-
-        let destinationFolderURL = FileManager.default.temporaryDirectory.appendingPathComponent("Export")
-        let destinationURL = destinationFolderURL.appendingPathComponent("Room.usdz")
-        let capturedRoomURL = destinationFolderURL.appendingPathComponent("Room.json")
-
+    @IBAction func cancelScanning(_ sender: UIBarButtonItem) {
+        navigationController?.dismiss(animated: true)
+    }
+    
+    // Export the USDZ output by specifying the `.parametric` export option.
+    // Alternatively, `.mesh` exports a nonparametric file and `.all`
+    // exports both in a single USDZ.
+    @IBAction func exportResults(_ sender: UIButton) {
+        let destinationFolderURL = FileManager.default.temporaryDirectory.appending(path: "Export")
+        let destinationURL = destinationFolderURL.appending(path: "Room.usdz")
+        let capturedRoomURL = destinationFolderURL.appending(path: "Room.json")
         do {
             try FileManager.default.createDirectory(at: destinationFolderURL, withIntermediateDirectories: true)
             let jsonEncoder = JSONEncoder()
             let jsonData = try jsonEncoder.encode(finalResults)
             try jsonData.write(to: capturedRoomURL)
-            try finalResults.export(to: destinationURL, exportOptions: .parametric)
-
+            try finalResults?.export(to: destinationURL, exportOptions: .parametric)
+            
             let activityVC = UIActivityViewController(activityItems: [destinationFolderURL], applicationActivities: nil)
             activityVC.modalPresentationStyle = .popover
+            
             present(activityVC, animated: true, completion: nil)
-            activityVC.popoverPresentationController?.sourceView = exportButton
+            if let popOver = activityVC.popoverPresentationController {
+                popOver.sourceView = self.exportButton
+            }
         } catch {
-            print("Export error: \(error)")
+            print("Error = \(error)")
+        }
+    }
+    
+    private func setActiveNavBar() {
+        UIView.animate(withDuration: 1.0, animations: {
+            self.cancelButton?.tintColor = .white
+            self.doneButton?.tintColor = .white
+            self.exportButton?.alpha = 0.0
+        }, completion: { complete in
+            self.exportButton?.isHidden = true
+        })
+    }
+    
+    private func setCompleteNavBar() {
+        self.exportButton?.isHidden = false
+        UIView.animate(withDuration: 1.0) {
+            self.cancelButton?.tintColor = .systemBlue
+            self.doneButton?.tintColor = .systemBlue
+            self.exportButton?.alpha = 1.0
         }
     }
 }
+
